d09f1ad267430b1dc70ed35c1ad8586f6d55584d
jaikio <98924107+jaikio@users.noreply.github.com>
2022-08-13 19:13:48 +0200

    Add files via upload
    html: basic structure, 
    css: basic structure,
    js: enabled input fields, enabled keyboard input for numbers, enabled display of inputs

---
+++ script.js
@@ -0,0 +1,104 @@
+let score = "";
+let calculation = "";
+function refreshScore() {
+  document.querySelector(".result").textContent = score;
+}
+//numbers buttons
+document.querySelector(".num1").addEventListener("click", function () {
+  score = score + "1";
+  refreshScore();
+});
+
+document.querySelector(".num2").addEventListener("click", function () {
+  score = score + "2";
+  refreshScore();
+});
+
+document.querySelector(".num3").addEventListener("click", function () {
+  score = score + "3";
+  refreshScore();
+});
+
+document.querySelector(".num4").addEventListener("click", function () {
+  score = score + "4";
+  refreshScore();
+});
+
+document.querySelector(".num5").addEventListener("click", function () {
+  score = score + "5";
+  refreshScore();
+});
+
+document.querySelector(".num6").addEventListener("click", function () {
+  score = score + "6";
+  refreshScore();
+});
+
+document.querySelector(".num7").addEventListener("click", function () {
+  score = score + "7";
+  refreshScore();
+});
+
+document.querySelector(".num8").addEventListener("click", function () {
+  score = score + "8";
+  refreshScore();
+});
+
+document.querySelector(".num9").addEventListener("click", function () {
+  score = score + "9";
+  refreshScore();
+});
+
+document.querySelector(".num0").addEventListener("click", function () {
+  score = score + "0";
+  refreshScore();
+});
+
+//action buttons
+document.querySelector(".multiply").addEventListener("click", function () {
+  score = score + "x";
+  refreshScore();
+});
+
+document.querySelector(".subtract").addEventListener("click", function () {
+  score = score + "-";
+  refreshScore();
+});
+
+document.querySelector(".add").addEventListener("click", function () {
+  score = score + "+";
+  refreshScore();
+});
+
+document.querySelector(".divide").addEventListener("click", function () {
+  score = score + ":";
+  refreshScore();
+});
+
+document.querySelector(".reset").addEventListener("click", function () {
+  score = "";
+  refreshScore();
+});
+
+document.querySelector(".inverse").addEventListener("click", function () {
+  score = -score;
+  refreshScore();
+});
+
+document.querySelector(".percentage").addEventListener("click", function () {
+  score = Number(score) * 100;
+  refreshScore();
+});
+
+document.querySelector(".comma").addEventListener("click", function () {
+  score = score + ".";
+  refreshScore();
+});
+
+document.addEventListener("keypress", function (event) {
+  let calcInput = event.key;
+  if (Number(calcInput) >= 0 && Number(calcInput) <= 9) {
+    score = score + calcInput;
+    refreshScore();
+  }
+});
